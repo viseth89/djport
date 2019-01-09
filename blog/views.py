1dd5f15dbebc7d0b0d8f7c0e1abf16728e20502a
viseth89 <viseth89@gmail.com>
2019-01-09 07:34:55 -0600

    crated models for blog, testing blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Blog
 
 # Create your views here.
+def index(request):
+    return render(request, 'blog/index.html')
+
+def blogDetail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    context = {
+        'blog':blog
+    }
+    return render(request, 'pages/blog_detail.html', context)
--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
-    # path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     # path('jobs', include('jobs.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
