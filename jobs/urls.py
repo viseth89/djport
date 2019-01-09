574f975da89e6d7a02c7f7349cd6c073900f4fdb
viseth89 <viseth89@gmail.com>
2019-01-09 12:20:44 -0600

    working on links and index page

--- jobs/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.jobIndex, name='jobIndex'),
-    path('<int:job_id>/', views.jobDetail, name='jobDetail')
-]
---
+++ work/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Work
+
+
+def workIndex(request):
+    work = Work.objects
+
+    return render(request, 'work/work_index.html', {'work':work})
+
+def workDetail(request, work_id):
+    work = get_object_or_404(Work, pk=work_id)
+    context = {
+        'work':work
+    }
+    return render(request, 'work/work_detail.html', context)
+
