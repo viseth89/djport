1162ed57985c79d83745f6eaaf462e291dc1be32
viseth89 <viseth89@gmail.com>
2019-01-08 18:58:38 -0600

    tweaked ignore file and switched over to postgres db on local from sqllite

--- work/models.py
+++ work/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Work(models.Model):
+    image = models.ImageField(upload_to='images/')
+    highlights = models.CharField(max_length=200)
+    description = models.TextField() 
