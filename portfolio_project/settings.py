6033ae8ce4a7d12cd4985fe17b53a22c9f0fe361
viseth89 <viseth89@gmail.com>
2019-01-09 10:51:45 -0600

    working partials, base, footer, nav, index and about, gonna work on blogs next

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'btre/static')
+    os.path.join(BASE_DIR, 'portfolio_project/static')
 ]
 
 # Media Folder Settings
