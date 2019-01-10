5cedd0bd94a90b0f36984483725120bef132d52d
viseth89 <viseth89@gmail.com>
2019-01-09 19:11:41 -0600

    problems with deployment

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.blogIndex, name='blogIndex'),
-    path('<int:blog_id>/', views.blogDetail, name='blogDetail')
-]
