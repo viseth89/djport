d2a559ac8e64879123c25f2ab4f26ac01e8ce65b
viseth89 <viseth89@gmail.com>
2019-01-10 18:14:29 -0600

    made dynamic links in work, tweaked blog view

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,15 @@ class Blog(models.Model):
   title = models.CharField(max_length=255)
   pub_date = models.DateTimeField()
   body = models.TextField()
-  image = models.ImageField(upload_to='images/') 
+  image = models.ImageField(upload_to='images/')
+  
+
+  # Return titles in admin panel
+  def __str__(self):
+    return self.title
+  # Provide option to display shortened body text to keep styles uniform and consistent
+  def summary(self):
+    return self.body[:100]
+  # Provide option to display date/time in a 'pretty' way
+  def pub_date_pretty(self):
+    return self.pub_date.strftime('%b %e %Y')
--- work/models.py
+++ work/models.py
@@ -5,3 +5,4 @@ class Work(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
     desc = models.TextField()
+    address = models.CharField(max_length=255, default='www.google.com') 
