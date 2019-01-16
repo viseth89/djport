8b59c71cb017419568da1a7195fae574fe9dfa63
viseth89 <viseth89@gmail.com>
2019-01-16 16:20:00 -0600

    added bootstrap b, work and index now show title of work on hover

--- demos/urls.py
+++ demos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bootstrap4a',views.bootstrap4a, name='bootstrap4a')
+    path('bootstrap4a', views.bootstrap4a, name='bootstrap4a'),
+    path('bootstrap4b', views.bootstrap4b, name='bootstrap4b')
 ]
