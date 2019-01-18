368b830f2b603a32b05621f9a4c4e2bce2075256
viseth89 <viseth89@gmail.com>
2019-01-18 10:37:49 -0600

    added project 4, made urls work with django

--- demos/urls.py
+++ demos/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('bootstrap4a', views.bootstrap4a, name='bootstrap4a'),
     path('bootstrap4b', views.bootstrap4b, name='bootstrap4b'),
     path('bootstrap4c', views.bootstrap4c, name='bootstrap4c'),
+    path('bootstrap4d', views.bootstrap4d, name='bootstrap4d'),
+    path('bootstrap4d/about', views.bootstrap4dAbout, name='bootstrap4dAbout'),
+    path('bootstrap4d/blog', views.bootstrap4dBlog, name='bootstrap4dBlog'),
+    path('bootstrap4d/contact', views.bootstrap4dContact, name='bootstrap4dContact'),
+    path('bootstrap4d/services', views.bootstrap4dServices, name='bootstrap4dServices'),
     path('jsjq1',views.jsjq1,name='jsjq1'),
 ]
--- demos/views.py
+++ demos/views.py
@@ -9,5 +9,20 @@ def bootstrap4b(request):
 def bootstrap4c(request):
     return render(request, 'demos/bootstrap4c/index.html')
 
+def bootstrap4d(request):
+    return render(request, 'demos/bootstrap4d/index.html')
+
+def bootstrap4dAbout(request):
+    return render(request, 'demos/bootstrap4d/about.html')
+
+def bootstrap4dBlog(request):
+    return render(request, 'demos/bootstrap4d/blog.html')
+
+def bootstrap4dContact(request):
+    return render(request, 'demos/bootstrap4d/contact.html')
+
+def bootstrap4dServices(request):
+    return render(request, 'demos/bootstrap4d/services.html')
+
 def jsjq1(request):
     return render(request, 'demos/jsjq1/index.html')
