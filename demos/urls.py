1face59ad2686a19823b4019d5109be432c00408
viseth89 <viseth89@gmail.com>
2019-01-21 14:40:41 -0600

    fixed links for blogen

--- demos/urls.py
+++ demos/urls.py
@@ -7,18 +7,21 @@ urlpatterns = [
     path('bootstrap4b', views.bootstrap4b, name='bootstrap4b'),
     path('bootstrap4c', views.bootstrap4c, name='bootstrap4c'),
     path('bootstrap4d', views.bootstrap4d, name='bootstrap4d'),
+
     path('bootstrap4d/about', views.bootstrap4dAbout, name='bootstrap4dAbout'),
     path('bootstrap4d/blog', views.bootstrap4dBlog, name='bootstrap4dBlog'),
     path('bootstrap4d/contact', views.bootstrap4dContact, name='bootstrap4dContact'),
     path('bootstrap4d/services', views.bootstrap4dServices, name='bootstrap4dServices'),
+    
     path('bootstrap4e', views.bootstrap4e, name='bootstrap4e'),
+
     path('bootstrap4f', views.bootstrap4f, name='bootstrap4f'),
-    path('bootstrap4f/categories', views.bootstrap4f, name='bootstrap4fCategories'),
-    path('bootstrap4f/details', views.bootstrap4f, name='bootstrap4fDetails'),
-    path('bootstrap4f/login', views.bootstrap4f, name='bootstrap4fLogin'),
-    path('bootstrap4f/posts', views.bootstrap4f, name='bootstrap4fPosts'),
-    path('bootstrap4f/profile', views.bootstrap4f, name='bootstrap4fProfile'),
-    path('bootstrap4f/settings', views.bootstrap4f, name='bootstrap4fSettings'),
-    path('bootstrap4f/users', views.bootstrap4f, name='bootstrap4fUsers'),
+    path('bootstrap4f/categories', views.bootstrap4fCategories, name='bootstrap4fCategories'),
+    path('bootstrap4f/details', views.bootstrap4fDetails, name='bootstrap4fDetails'),
+    path('bootstrap4f/login', views.bootstrap4fLogin, name='bootstrap4fLogin'),
+    path('bootstrap4f/posts', views.bootstrap4fPosts, name='bootstrap4fPosts'),
+    path('bootstrap4f/profile', views.bootstrap4fProfile, name='bootstrap4fProfile'),
+    path('bootstrap4f/settings', views.bootstrap4fSettings, name='bootstrap4fSettings'),
+    path('bootstrap4f/users', views.bootstrap4fUsers, name='bootstrap4fUsers'),
     path('jsjq1/settings',views.jsjq1,name='jsjq1'),
 ]
