3e46280865e8801953d5b674b65071b103956d5f
viseth89 <viseth89@gmail.com>
2019-01-21 14:22:31 -0600

    blogen tweak and page fixes

--- demos/views.py
+++ demos/views.py
@@ -27,5 +27,29 @@ def bootstrap4dServices(request):
 def bootstrap4e(request):
     return render(request, 'demos/bootstrap4e/index.html')
 
+def bootstrap4f(request):
+    return render(request, 'demos/bootstrap4f/index.html')
+
+def bootstrap4fCategories(request):
+    return render(request, 'demos/bootstrap4f/categories.html')
+
+def bootstrap4fDetails(request):
+    return render(request, 'demos/bootstrap4f/details.html')
+
+def bootstrap4fLogin(request):
+    return render(request, 'demos/bootstrap4f/login.html')
+
+def bootstrap4fPosts(request):
+    return render(request, 'demos/bootstrap4f/posts.html')
+
+def bootstrap4fProfile(request):
+    return render(request, 'demos/bootstrap4f/profile.html')
+
+def bootstrap4fSettings(request):
+    return render(request, 'demos/bootstrap4f/settings.html')
+
+def bootstrap4fUsers(request):
+    return render(request, 'demos/bootstrap4f/users.html')
+
 def jsjq1(request):
     return render(request, 'demos/jsjq1/index.html')
